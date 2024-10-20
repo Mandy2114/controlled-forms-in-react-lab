4dc68223a35002816e273214c4a789a17a6fcf74
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-20 02:37:18 -0400

    Map through your books

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -5,8 +5,8 @@ const Bookself = () => {
 
   const [newBook, setNewBook] = useState(
     {
-    title: "Shadow Spinner",
-    author: "Susan Fletcher",
+      title: "Shadow Spinner",
+      author: "Susan Fletcher",
     },
     {
       title: "The Otherworldlies",
@@ -31,31 +31,41 @@ const Bookself = () => {
   };
 
   return (
+    <>
+      <div className="bookshelfDiv">
+        <div className="formDiv">
+          <h3>Add a book</h3>
 
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a book</h3>
-        < form onSubmit={handleSubmit}>
-          <label htmlFor="title">Title</label>
-          <input 
-            type="text"
-            id="title"
-            name="title"
-            value={newBook.title}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="title">Author</label>
-          <input
-            type="text"
-            id="author"
-            name="author"
-            value={newBook.author}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Add a book</button>
-        </form>
-  </div>
-</div>
-  )
+          < form onSubmit={handleSubmit}>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+            />
+            <label htmlFor="title">Author</label>
+            <input
+              type="text"
+              id="author"
+              name="author"
+              value={newBook.author}
+              onChange={handleInputChange}
+            />
+            <button type="submit">Add a book</button>
+          </form>
+       </div>
+         <div className="bookCardDiv">
+            {book.map((book, index) => (
+              <div key={index} className="bookCard">
+                <h3>{book.title}</h3>
+                <p>by {book.author}</p>
+              </div>
+            ))}
+          </div>
+      </div> 
+    </>
+  );
 }
   
