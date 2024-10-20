3a23165c6c7faedd8d0b9872e706d28a1c4f3890
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-20 02:23:56 -0400

    Form creation

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -14,7 +14,7 @@ const Bookself = () => {
     }
   );
 
-  const handlInputChange = (event) => {
+  const handleInputChange = (event) => {
     setNewBook({
       ...newBook,
       [event.target.name]: event.target.value,
@@ -35,7 +35,25 @@ const Bookself = () => {
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a book</h3>
-    {/*form will go here */}
+        < form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input 
+            type="text"
+            id="title"
+            name="title"
+            value={newBook.title}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="title">Author</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={newBook.author}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Add a book</button>
+        </form>
   </div>
 </div>
   )
