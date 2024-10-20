91f175036cfe215923f2a2b1c751269b83b85912
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-20 02:08:06 -0400

    Define the initial state

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
 
+const Bookself = () => {
+  const [book, setbooks] = useState([]);
+
+  const [newBook, setNewBook] = useState(
+    {
+    title: "Shadow Spinner",
+    author: "Susan Fletcher",
+    },
+    {
+      title: "The Otherworldlies",
+      author: "Jennifer Anne Kogler",
+    }
+  );
+
+  return (
+
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a book</h3>
     {/*form will go here */}
   </div>
 </div>
+  )
+}
+  
